06d262bceb46d6dc2c1bc261ceb7c5eaca872845
Michael <michaelhv@uia.no>
2021-11-24 09:36:54 +0100

    Database connection is working

--- platnsio.py
+++ platnsio.py
@@ -1,8 +1,8 @@
-import time
-from ISStreamer.Streamer import Streamer
-from board import SCL, SDA
-import busio
-from adafruit_seesaw.seesaw import Seesaw
+import time from ISStreamer.Streamer
+import Streamer from board
+import SCL, SDA
+import busio from adafruit_seesaw.seesaw
+import Seesaw
 import mysql.connector
 
 # --------- User Settings ---------
@@ -14,9 +14,9 @@ MINUTES_BETWEEN_READS = 0.25
 # ---------------------------------
 
 # ------- Database Connection ----
-database = mysql.connector.connect("localhost", "plantsiodb", "root")
-cursor = connection.cursor(prepared=True)
-sql_insert_query = "INSERT INTO plantsIOTable(plant_moisture, plant_temp) VALUES(%s,%S)"
+database_host="localhost"
+database_name="plantsiodb"
+database_user="root"
 # ---------------------------------
 
 # ------- InitialState+Sensors ----
@@ -30,15 +30,17 @@ while True:
 
     # read temperature from the temperature sensor and convert to F
     temp = ss.get_temp()
-    temp_f = format(temp * 9.0 / 5.0 + 32, ".2f")
 
     streamer.log(SENSOR_LOCATION_NAME + " Moisture", touch)
-    streamer.log(SENSOR_LOCATION_NAME + " Temperature", temp_f)
+    streamer.log(SENSOR_LOCATION_NAME + " Temperature", temp)
     streamer.flush()
-
-    sql_touple = (ss.moisture_read, ss.get_temp)
-    cursor.execute(sql_insert_query, sql_touple)
-    connection.commit()
+    try:
+        connection = mysql.connector.connect(host=database_host, database=database_name, user=database_user)
+        cursor = connection.cursor(prepared=True)
+        sql_insert_query = "INSERT INTO plantsIOTable(plant_moisture, plant_temp) VALUES(%s,%s)"
+        sql_touple = (touch, temp)
+        cursor.execute(sql_insert_query, sql_touple)
+        connection.commit()
 
     except mysql.connector.Error as error:
         print("Error message {} ".format(error))
@@ -46,7 +48,5 @@ while True:
         if connection.is_connected():
             cursor.close()
             connection.close()
-            print("MySQL connection is closed")
-
+            print("Database got the data")
     time.sleep(60*MINUTES_BETWEEN_READS)
-# -------------------------------
