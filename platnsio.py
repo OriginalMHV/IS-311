27589449ce55f0a1f65b2c2806859461b3a23b40
Michael <michaelhv@uia.no>
2021-12-02 12:09:16 +0100

    Fixed format + names

--- platnsio.py
+++ platnsio.py
@@ -1,21 +1,21 @@
-import time from ISStreamer.Streamer
-import Streamer from board
-import SCL, SDA
-import busio from adafruit_seesaw.seesaw
-import Seesaw
+import time
+from ISStreamer.Streamer import Streamer
+from board import SCL, SDA
+import busio
+from adafruit_seesaw.seesaw import Seesaw
 import mysql.connector
 
 # --------- User Settings ---------
 SENSOR_LOCATION_NAME = "House Plant"
 BUCKET_NAME = "House Plant"
 BUCKET_KEY = "soilsensor"
-ACCESS_KEY = "ist_8u0aHz7qqYO-w8HJaUDyLPIGWOyTcp3p"
-MINUTES_BETWEEN_READS = 0.25
+ACCESS_KEY = "ENTER KEY HERE"
+MINUTES_BETWEEN_READS = 0.1
 # ---------------------------------
 
 # ------- Database Connection ----
 database_host="localhost"
-database_name="plantsiodb"
+database_name="plantsiotdb"
 database_user="root"
 # ---------------------------------
 
@@ -25,20 +25,24 @@ ss = Seesaw(i2c_bus, addr=0x36)
 streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
 
 while True:
-    # read moisture level through capacitive touch pad
-    touch = ss.moisture_read()
+# read moisture level through capacitive touch pad
+    moisture = ss.moisture_read()
+    moisture_p = format(moisture / 20)
 
-    # read temperature from the temperature sensor and convert to F
+# read temperature from the temperature sensor and convert to C
     temp = ss.get_temp()
+    temp_c = format(temp - 5,".2f") 
+    
+    print("The moisture is: " + moisture_p + "%, " + "and the temperature is: " + temp_c + "C.")
 
-    streamer.log(SENSOR_LOCATION_NAME + " Moisture", touch)
-    streamer.log(SENSOR_LOCATION_NAME + " Temperature", temp)
+    streamer.log(SENSOR_LOCATION_NAME + " Moisture", moisture_p)
+    streamer.log(SENSOR_LOCATION_NAME + " Temperature", temp_c)
     streamer.flush()
     try:
         connection = mysql.connector.connect(host=database_host, database=database_name, user=database_user)
         cursor = connection.cursor(prepared=True)
-        sql_insert_query = "INSERT INTO plantsIOTable(plant_moisture, plant_temp) VALUES(%s,%s)"
-        sql_touple = (touch, temp)
+        sql_insert_query = "INSERT INTO plantsiottable(plant_moisture, plant_temp) VALUES(%s,%s)"
+        sql_touple = (moisture_p, temp_c)
         cursor.execute(sql_insert_query, sql_touple)
         connection.commit()
 
@@ -48,5 +52,5 @@ while True:
         if connection.is_connected():
             cursor.close()
             connection.close()
-            print("Database got the data")
+            print("Database received: " + str(sql_touple) +"\n")
     time.sleep(60*MINUTES_BETWEEN_READS)
