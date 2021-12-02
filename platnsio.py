4a543ac0acabb9fee3a5ec01c96245847e90e738
Michael <michaelhv@uia.no>
2021-12-02 12:18:46 +0100

    Done with documentation

--- platnsio.py
+++ platnsio.py
@@ -1,9 +1,11 @@
+# --------- Libraries ------------
 import time
 from ISStreamer.Streamer import Streamer
 from board import SCL, SDA
 import busio
 from adafruit_seesaw.seesaw import Seesaw
 import mysql.connector
+# --------------------------------
 
 # --------- User Settings ---------
 SENSOR_LOCATION_NAME = "House Plant"
@@ -19,25 +21,34 @@ database_name="plantsiotdb"
 database_user="root"
 # ---------------------------------
 
-# ------- InitialState+Sensors ----
+# -- InitialState + Sensor data --
 i2c_bus = busio.I2C(SCL, SDA)
 ss = Seesaw(i2c_bus, addr=0x36)
 streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
 
 while True:
-# read moisture level through capacitive touch pad
+# Reads the moisture level through capacitive touch pad
     moisture = ss.moisture_read()
+# Formats the input from 200-2000 range to % for readability
     moisture_p = format(moisture / 20)
 
-# read temperature from the temperature sensor and convert to C
+# Reads the temperature from the temperature sensor 
     temp = ss.get_temp()
+# Format the temperature to .2 decimals and a small calibration (-5)
     temp_c = format(temp - 5,".2f") 
     
+# Prints out the moisture and temperature to terminal
     print("The moisture is: " + moisture_p + "%, " + "and the temperature is: " + temp_c + "C.")
 
+# Sends the data to InitialState 
     streamer.log(SENSOR_LOCATION_NAME + " Moisture", moisture_p)
     streamer.log(SENSOR_LOCATION_NAME + " Temperature", temp_c)
     streamer.flush()
+# ---------------------------------
+
+# -- Database connection and queries --
+# Sends the data to local MySQL database, the script is included. (plantsiotdb.sql)
+# Prepared statement to send the data to the database and error handling.
     try:
         connection = mysql.connector.connect(host=database_host, database=database_name, user=database_user)
         cursor = connection.cursor(prepared=True)
@@ -54,3 +65,4 @@ while True:
             connection.close()
             print("Database received: " + str(sql_touple) +"\n")
     time.sleep(60*MINUTES_BETWEEN_READS)
+# --------------------------------------
